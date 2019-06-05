9adefd4a2998a11857a82d3e2812eed10d13e603
rpasqualone7 <highwater92@gmail.com>
2019-06-04 23:45:45 -0400

    Updates

---
+++ precache-manifest.608764ec5df1835110d4f25f2c98620d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5709475f0b9b1d318d16b93d2b9c25",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "2d07d7c4d53948175308",
+    "url": "/clicky-game/static/css/main.fd3856f6.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "2d07d7c4d53948175308",
+    "url": "/clicky-game/static/js/main.4b442d05.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
